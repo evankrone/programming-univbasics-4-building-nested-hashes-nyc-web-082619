45c707a4f90718e96d01001a7efc4d3eb6379447
Evan Krone <evankrone1@gmail.com>
2019-08-19 19:55:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :name3 => {},
+	      :b_and_o_railroad => {},
 	      :name4 => {}
 	    },
 	    :rent_in_dollars => {
