d287d6404ebff17ab857838690055dd326d4e042
Evan Krone <evankrone1@gmail.com>
2019-08-19 19:53:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_piece_owned => 50,
+	      :two_pieces_owned => 50,
 	      :ownership3 => {},
 	      :ownership4 => {}
 	       }
