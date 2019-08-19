9737bdbc584bc648ea7370ff9069bef326f013ce
Evan Krone <evankrone1@gmail.com>
2019-08-19 19:42:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,10 +27,8 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :ownership1 => {},
-	      :ownership2 => {},
-	      :ownership3 => {},
-	      :ownership4 => {}
+	      :names => {}
+	     
 	    }
 	  }
 	}
